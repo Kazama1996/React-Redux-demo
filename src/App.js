9208ecae1ea29fafab610e7679bd5c2ca8f19843
Yuta Kazama <Y.Kazama1996@gmail.com>
2024-03-14 00:23:08 +0800

    create src/component & src/store

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./component/Profile";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userSlice from "./store/userSlice";
+
+const store = configureStore({
+  reducer: {
+    // key : value
+    user: userSlice,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Profile />
+      </div>
+    </Provider>
   );
 }
 
